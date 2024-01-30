215c3c957fa765e6191296b4c7d3a08ed5f72736
Jae Gifford <jae.giff@outlook.com>
2024-01-29 21:16:45 -0800

    sliding feature working

--- src/components/slideshow/SlidingFeature.tsx
+++ src/components/slideshow/SlidingFeature.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { motion, useAnimate } from 'framer-motion';
 import FeaturedElement from './FeaturedElement';
 import { PublicationType } from '@/types/data';
 import uniqid from 'uniqid';
+import LeftChevron from './LeftChevron';
+import RightChevron from './RightChevron';
 type SlidingFeatureProps = {
   data: PublicationType[];
 };
@@ -11,36 +13,37 @@ function SlidingFeature({ data }: SlidingFeatureProps) {
   // opacity also needs to be smaller
   const [scope, animate] = useAnimate();
   const [current, setCurrent] = useState(Math.floor(data.length / 2));
+  const [translateX, setTranslateX] = useState(0);
+
   const handleChangeIndex = (clickedIndex: number) => {
-    console.log(clickedIndex, current, data.length);
     if (clickedIndex === current) return;
-    if (
-      clickedIndex === 0 ||
-      current === 0 ||
-      current === 1 ||
-      clickedIndex === data.length - 1 ||
-      current === data.length - 1
-    ) {
-      console.log('restricted');
-      return setCurrent(clickedIndex);
-    }
+
     const distance = current - clickedIndex;
-    console.log('translating', distance);
 
     const travel = distance * 30;
-    console.log(distance);
-    animate(scope.current, { x: `${travel}vw` });
+    setTranslateX((prev) => prev + travel);
     setCurrent(clickedIndex);
   };
-  useEffect(() => {
-    console.log(current);
-  }, [current]);
+
+  const incIndex = () => {
+    if (current !== data.length - 1) {
+      setTranslateX((prev) => prev + 30);
+      setCurrent((prev) => prev + 1);
+    }
+  };
+  const decIndex = () => {
+    if (current !== 0) {
+      setTranslateX((prev) => prev - 30);
+      setCurrent((prev) => prev - 1);
+    }
+  };
+
   return (
     <span className='w-full h-full flex justify-center items-center border-2'>
-      <div className='flex justify-center items-center border-blue-500 border-2 w-[90vw]'>
+      <div className='flex justify-center items-center w-[90vw] overflow-hidden'>
         <motion.div
-          ref={scope}
-          className='border-2 border-red-500 flex justify-center w-fit items-center'
+          animate={{ x: `${translateX}vw` }}
+          className='flex justify-center w-fit items-center'
         >
           {data.map((publication, i) => (
             <div
