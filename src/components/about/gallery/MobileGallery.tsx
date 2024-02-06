a7bf56689877b572926b32cdb44a7e795d376ac0
Jae Gifford <jae.giff@outlook.com>
2024-02-05 16:38:42 -0800

    mobile gallery render improved

--- src/components/about/gallery/MobileGallery.tsx
+++ src/components/about/gallery/MobileGallery.tsx
@@ -10,7 +10,7 @@ function MobileGallery({ data }: MobileGalleryProps) {
       {data.map((img) => (
         <div
           key={uniqid()}
-          className='bg-white p-1 min-w-[70vw] flex justify-center items-center'
+          className='bg-white p-1 min-w-[70vw] max-h-[300px] overflow-hidden flex justify-center items-center'
         >
           <Image src={img.src} alt='gallery image' height={600} width={600} />
         </div>
