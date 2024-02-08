e0c63179eaf381b546f3977ee4f39092f624f996
Jae Gifford <jae.giff@outlook.com>
2024-02-07 22:58:33 -0800

    added padding to contact page header

--- src/components/contact/ContactTitle.tsx
+++ src/components/contact/ContactTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ContactTitle() {
   return (
-    <div>
+    <div className='p-5'>
       <h1 className='text-7xl p-5'>Let&apos;s get in touch.</h1>
     </div>
   );
